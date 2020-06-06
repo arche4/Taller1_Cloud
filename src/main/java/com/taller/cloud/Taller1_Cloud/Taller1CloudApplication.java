6f4ca180c5dc6a5f84b29973e4d3caea67696c60
Manuela Jimenez <manuela_jimenez23141@elpoli.edu.co>
2020-06-06 15:25:12 -0500

    Primer commit

---
+++ src/main/java/com/taller/cloud/Taller1_Cloud/Taller1CloudApplication.java
@@ -0,0 +1,13 @@
+package com.taller.cloud.Taller1_Cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1CloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taller/cloud/Taller1_Cloud/Taller1CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.cloud.Taller1_Cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
